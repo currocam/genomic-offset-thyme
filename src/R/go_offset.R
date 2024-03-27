e841ba4e6b6ba603a7e6b3efa71054bc1232a32b
currocam <campuzanocurro@gmail.com>
2024-03-27 14:12:18 +0100

    Apply linter

--- src/R/go_offset.R
+++ src/R/go_offset.R
@@ -1,14 +1,14 @@
-  compute_k <- function(Y, threshold = 0.05) {
-    infile <- tempfile(fileext = ".geno")
-    pca.res <- pca(write.geno(Y, output.file = infile))
-    m1 <- tracy.widom(pca.res)
-    dirname(infile) |>
-      list.files(sub('\\.geno$', '', basename(infile)), full.names = TRUE) |>
-      unlink(recursive = TRUE)
-    project <- sub('\\.geno$', '', basename(infile))
-    remove.pcaProject(paste0(project, ".pcaProject"))
-    sum(m1$pvalues < threshold)
-  }
+compute_k <- function(Y, threshold = 0.05) {
+  infile <- tempfile(fileext = ".geno")
+  pca.res <- pca(write.geno(Y, output.file = infile))
+  m1 <- tracy.widom(pca.res)
+  dirname(infile) |>
+    list.files(sub('\\.geno$', '', basename(infile)), full.names = TRUE) |>
+    unlink(recursive = TRUE)
+  project <- sub('\\.geno$', '', basename(infile))
+  remove.pcaProject(paste0(project, ".pcaProject"))
+  sum(m1$pvalues < threshold)
+}
 
 go_genetic_gap <- function(Y, X, X.pred, snps.set){
   Y <- Y[, snps.set]
