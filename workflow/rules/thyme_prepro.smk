49d6094b1489f8337e569652f5f9dcb5aec68f46
currocam <campuzanocurro@gmail.com>
2024-03-27 16:22:55 +0100

    Update prepro to use latest assembly

--- workflow/rules/thyme_prepro.smk
+++ workflow/rules/thyme_prepro.smk
@@ -1,27 +1,29 @@
 rule go_preprocess:
     input:
-       bedfile = "data/genos01_02_2024/tVulg_thyme2016CoreSampling_maf01_geno005_maf0.1_geno0.05_imiss0.1_r20.1_prunned.bed",
-       annotation_tsv = "data/genos01_02_2024/tVulg_thyme2016CoreSampling_maf01_geno005_maf0.1_geno0.05_imiss0.1_r20.1_prunned.snps_annotation.tsv",
-       sample_tsv = "data/genos01_02_2024/thymus_2016_sample_info_clean233samples.tsv",
+       prunned = "data/genos27_03_2024/tVulgragtag_thyme2016CoreSampling_maf0.1_geno0.05_imiss0.1_r20.1_prunned_hetremove.bed",
+       unprunned = "data/genos27_03_2024/tVulgragtag_thyme2016CoreSampling_maf0.1_geno0.05_imiss0.1_hetremove.bed",
+       annotation_tsv = "data/genos27_03_2024/tVulgragtag_thyme2016CoreSampling_maf0.1_geno0.05_imiss0.1_hetremove.snps_annotation.tsv",
+       sample_tsv = "data/genos27_03_2024/thymus_2016_sample_info_clean231samples_hetremove.tsv",
        environment_rds = "data/env_00_12_2019/EnvVariables_5PCs_dec2019.rds"
     output:
-        "steps/genomic_offset/genos01_02_2024_prepro.Rds",
+        "steps/genomic_offset/genos27_03_2024_prepro.Rds",
     log:
-        "logs/genomic_offset/genos01_02_2024_prepro.log",
+        "logs/genomic_offset/genos27_03_2024_prepro.log",
     resources:
         mem_mb=3000,
         runtime=10,
     script:
         "../scripts/prepro_thyme.jl"
 
+
 rule ee_ERA5:
     input:
-        metadata = "data/genos01_02_2024/thymus_2016_sample_info_clean233samples.tsv",
+        metadata = "data/genos27_03_2024/thymus_2016_sample_info_clean231samples_hetremove.tsv",
         script = "scripts/05_earth_engine_ERA5.py"
     output:
-        "results/ecological/ERA5_233samples.csv",
+        "results/ecological/ERA5_231samples.csv",
     log:
-        "logs/ecological/ERA5_233samples.log",
+        "logs/ecological/ERA5_231samples.log",
     resources:
         mem_mb=1000,
         runtime=5,
--- workflow/scripts/prepro_thyme.jl
+++ workflow/scripts/prepro_thyme.jl
@@ -2,39 +2,49 @@ using Pkg
 Pkg.activate(".")
 using SnpArrays, CSV, DataFrames, RCall
 R"library(tidyverse)"
-function run(bedfile, annotation_tsv, sample_tsv, environment_rds, outfile)
-  genome = convert(Matrix{Float64}, SnpArray(bedfile); impute=true)
 
+function plink(bedfile)
+  mat = convert(Matrix{Float64}, SnpArray(bedfile); impute=true)
+  # Remove extension
+  basename = replace(bedfile, r"\.bed$" => "");
+  bim = DataFrame(CSV.File("$basename.bim"; header=false))
+  fam = DataFrame(CSV.File("$basename.fam"; header=false))
+  sample_names = fam[:, 2]
+  snp_names = bim[:, 2]
+  return mat, sample_names, snp_names
+end
+
+function run(prunned, unprunned, annotation_tsv, sample_tsv, environment_rds, outfile)
+  prunned, prunned_samples, prunned_snps = plink(prunned)
+  unprunned, unprunned_samples, unprunned_snps = plink(unprunned)
   gene_ann = DataFrame(
     CSV.File(annotation_tsv; header=["Chromosome", "Position", "Id", "Gene"])
   )
-  @assert(size(genome, 2) == size(gene_ann, 1))
-
   metadata = DataFrame(CSV.File(sample_tsv))
-  @assert(size(genome, 1) == size(metadata, 1))
-
-  environment = innerjoin(metadata, rcopy(R"read_rds($environment_rds)"), on=:site_id)
+  environment = leftjoin(metadata, rcopy(R"read_rds($environment_rds)"), on=:site_id)
   select!(environment, Not(names(metadata)))
-  @assert(size(genome, 1) == size(environment, 1))
 
-  @rput genome
-  @rput gene_ann
-  @rput metadata
-  @rput environment
-
+  @rput prunned prunned_samples prunned_snps unprunned unprunned_samples unprunned_snps gene_ann metadata environment
   R"""
+  rownames(prunned) <- prunned_samples
+  colnames(prunned) <- prunned_snps
+  rownames(unprunned) <- unprunned_samples
+  colnames(unprunned) <- unprunned_snps
   res <- list(
-    genome = genome,
+    prunned = prunned,
+    unprunned = unprunned,
     gene_ann = gene_ann,
     metadata = metadata,
-    environment = environment
+    environment = environment,
+    info = "prunned contains SNPs prunned by LD R2 > 0.1. Gene annotation is for the unprunned genome. Metadata is info for the samples, and environment is the environmental data from Thomas."
   )
   write_rds(res, $outfile)
   """
 end
 
 run(
-  snakemake.input["bedfile"],
+  snakemake.input["prunned"],
+  snakemake.input["unprunned"],
   snakemake.input["annotation_tsv"],
   snakemake.input["sample_tsv"],
   snakemake.input["environment_rds"],
