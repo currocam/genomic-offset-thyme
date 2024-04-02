6c52ea650a7272a90796c5cb29c2274c9fabf56b
currocam <campuzanocurro@gmail.com>
2024-04-02 14:27:00 +0200

    Add confounded files

--- scripts/06_counfounded_env.R
+++ scripts/06_counfounded_env.R
@@ -9,32 +9,33 @@ handle_simulation <- function(file){
   Y <- data$Genotype
   # Prepare env data
   X <- matrix(c(
-    data[["Current env 1"]], data[["Current env 2"]],
-    rnorm(100)
-  ),ncol=3)
+    data[["Current env 1"]], data[["Current env 2"]]
+  ),ncol=2) |>
+    cbind(replicate(100, rnorm(100)))
   X.pred <-  matrix(c(
-    data[["Future env 1"]], data[["Future env 2"]],
-    rnorm(100)
-  ),ncol=3)
+    data[["Future env 1"]], data[["Future env 2"]]
+  ),ncol = 2) |>
+    cbind(replicate(100, rnorm(100)))
   # Causal offset
   causal_loci <- c(data[["Index QTLs 1"]])
   causal <- go_genetic_gap(Y, X[,1], X.pred[,1], causal_loci)
-  causal_one_random <- go_genetic_gap(Y, X[,c(1, 3)], X.pred[,c(1, 3)], causal_loci)
   causal_one_counfounded <- go_genetic_gap(Y, X[,c(1, 2)], X.pred[,c(1, 2)], causal_loci)
   # All SNPs
   empirical <- go_genetic_gap(Y, X[,1], X.pred[,1], 1:ncol(Y))
-  empirical_one_random <- go_genetic_gap(Y, X[,c(1, 3)], X.pred[,c(1, 3)], 1:ncol(Y))
   empirical_one_counfounded <- go_genetic_gap(Y, X[,c(1, 2)], X.pred[,c(1, 2)], 1:ncol(Y))
-  tibble(
+  res <- tibble(
     current_fitness = data[["Current fitness"]],
     future_fitness = data[["Future fitness"]],
     causal_offset = causal,
-    causal_one_random_offset = causal_one_random,
-    causal_one_counfounded_offset = causal_one_counfounded,
+    causal_counfounded_offset = causal_one_counfounded,
     empirical_offset = empirical,
-    empirical_one_random_offset = empirical_one_random,
-    empirical_one_counfounded_offset = empirical_one_counfounded,
+    empirical_counfounded_offset = empirical_one_counfounded,
   )
+  walk(seq(1, 70, by = 5), \(index) {
+    res[paste0("causal_random_", index, "_offset")] <<- go_genetic_gap(Y, X[,c(1, 3:(2+index))], X.pred[,c(1, 3:(2+index))], causal_loci)
+    res[paste0("causal_random_", index, "_offset")] <<- go_genetic_gap(Y, X[,c(1, 3:(2+index))], X.pred[,c(1, 3:(2+index))], 1:ncol(Y))
+  })
+  res
 }
 
 run <- function() {
