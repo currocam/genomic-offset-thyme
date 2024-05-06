9e4dec472cd05bfcc8f02e549f168f401aeeada3
currocam <campuzanocurro@gmail.com>
2024-05-06 08:47:24 +0200

    Add boots

--- scripts/02_local_adaptation_scenarios_bootstraps.jl
+++ scripts/02_local_adaptation_scenarios_bootstraps.jl
@@ -1,132 +1,9 @@
-using RCall, GenomicOffsets, Statistics, DataFrames, MultipleTesting, CSV, Dates, Distributed
+using RCall, GenomicOffsets, Statistics, DataFrames, CSV, Dates, Random, MultipleTesting, LinearAlgebra
 R"library(tidyverse)"
 
-function log(x...)
+function logging(x...)
     println(Dates.now(), " ", join(x, " ")...)
     flush(stderr)
-end
-
-function empirical_genomic_offset(Y::Matrix{T1}, X::Matrix{T2}, Xstar::Matrix{T2}; scale=true, λ=0.0001, FTest_FDR=0.1, TW_threshold=0.01) where {T1<:Real,T2<:Real}
-    if scale
-        mx = mean(X, dims=1)
-        sdx = std(X, dims=1)
-        X = (X .- mx) ./ sdx
-        Xstar = (Xstar .- mx) ./ sdx
-    end
-    Y = Y .- mean(Y, dims=1)
-    # Undefined offset matrix
-    K = latent_factors_tracy_widom(Y, TW_threshold, false)
-    model = RidgeLFMM(Y, X, K, λ; center=false)
-    pvalues = LFMM_Ftest(model, Y, X) |> vec
-    qvalues = MultipleTesting.adjust(pvalues, BenjaminiHochberg())
-    putative_loci = findall(qvalues .< FTest_FDR)
-    Bt = model.Bt[:, putative_loci]
-    offsets = geometric_genomic_offset(Bt, X, Xstar)
-    return (offsets=offsets, K=K, putative_loci=putative_loci)
-end
-
-function causal_genomic_offset(Y::Matrix{T1}, X::Matrix{T2}, Xstar::Matrix{T2}, loci::Array{Int}; scale=true, λ=0.0001, TW_threshold=0.01) where {T1<:Real,T2<:Real}
-    if scale
-        mx = mean(X, dims=1)
-        sdx = std(X, dims=1)
-        X = (X .- mx) ./ sdx
-        Xstar = (Xstar .- mx) ./ sdx
-    end
-    Y = Y .- mean(Y, dims=1)
-    Y = Y[:, loci]
-    K = latent_factors_tracy_widom(Y, TW_threshold, false)
-    model = RidgeLFMM(Y, X, K, λ; center=false)
-    Bt = model.Bt
-    offsets = geometric_genomic_offset(Bt, X, Xstar)
-    return (offsets=offsets, K=K, loci=loci)
-end
-
-function bootstrap(Y::Matrix{T1}, X::Matrix{T2}, Xstar::Matrix{T2}, nboot::Int; scale=true, λ=0.0001, FTest_FDR=0.1, TW_threshold=0.01) where {T1<:Real,T2<:Real}
-    if scale
-        mx = mean(X, dims=1)
-        sdx = std(X, dims=1)
-        X = (X .- mx) ./ sdx
-        Xstar = (Xstar .- mx) ./ sdx
-    end
-    Y = Y .- mean(Y, dims=1)
-    # Undefined offset matrix
-    offsets = Array{Float64}(undef, size(X, 1), nboot)
-    latent_factors = Array{Int}(undef, nboot)
-    nputativeloci = Array{Int}(undef, nboot) 
-
-    for index in 1:nboot
-        idx = rand(1:size(Y, 2), size(Y, 2))
-        Ytemp = Y[:, idx]
-        K = latent_factors_tracy_widom(Ytemp, TW_threshold, false)
-        latent_factors[index] = K
-        model = RidgeLFMM(Ytemp, X, K, λ; center=false)
-        pvalues = LFMM_Ftest(model, Ytemp, X) |> vec
-        qvalues =  MultipleTesting.adjust(pvalues, BenjaminiHochberg())
-        putative_loci = findall(qvalues .< FTest_FDR)
-        nputativeloci[index] = length(putative_loci)
-        Bt = model.Bt[:, putative_loci]
-        offsets[:, index] = geometric_genomic_offset(Bt, X, Xstar)
-    end
-    return (offsets=offsets, latent_factors=latent_factors, nputativeloci=nputativeloci)
-
-end
-
-
-function grid_conditions(infile)
-    R"""
-    thresholds <- c(0.01, 0.05, 0.1)
-    lambdas <- c(0.00001)
-    conditions <- expand_grid(FTest_FDR = thresholds, TW_threshold = thresholds, lambda = lambdas) |>
-        mutate(file = $infile) |>
-        separate_wider_delim(file, "_", names = c("slim", "seed", NA, NA, "QTLs")) |>
-        mutate(
-            slim = basename(slim),
-            QTLs = str_remove_all(QTLs, ".Rds") |> as.integer(),
-            #m3.1 non local, m3.2 local, other NA
-            scenario = case_when(
-                grepl("m3.1", slim) ~ "Non local adaptation",
-                grepl("m3.2", slim) ~ "Local adaptation",
-                TRUE ~ NA_character_
-            )
-        )
-    """
-    @rget conditions
-    conditions[!, :empirical_K] .= NaN
-    conditions[!, :causal_K] .= NaN
-    conditions[!, :minconf_K] .= NaN
-    conditions[!, :maxconf_K] .= NaN
-    conditions[!, :empirical_nloci] .= NaN
-    conditions[!, :causal_nloci] .= NaN
-    conditions[!, :minconf_nloci] .= NaN
-    conditions[!, :maxconf_nloci] .= NaN
-    conditions[!, :empirical_cor_fitness] .= NaN
-    conditions[!, :causal_cor_fitness] .= NaN
-    conditions[!, :minconf_cor_fitness] .= NaN
-    conditions[!, :maxconf_cor_fitness] .= NaN
-    conditions
-end
-
-function handle_row!(row, Y, X, Xstar, minuslogfitness, causal_loci)
-    # Compute offsets
-    causal_offset = causal_genomic_offset(Y, X, Xstar, causal_loci; scale=true, λ=row.lambda, TW_threshold=row.TW_threshold)
-    empirical_offset = empirical_genomic_offset(Y, X, Xstar; scale=true, λ=row.lambda, FTest_FDR=row.FTest_FDR, TW_threshold=row.TW_threshold)
-    boots = bootstrap(Y, X, Xstar, 100; λ=row.lambda, scale=true, FTest_FDR=row.FTest_FDR, TW_threshold=row.TW_threshold)
-    # Latent factors
-    row.empirical_K = empirical_offset.K
-    row.causal_K = causal_offset.K
-    row.minconf_K = quantile(boots.latent_factors, 0.025)
-    row.maxconf_K = quantile(boots.latent_factors, 0.975)
-    # Loci
-    row.empirical_nloci = length(empirical_offset.putative_loci)
-    row.causal_nloci = length(causal_offset.loci)
-    row.minconf_nloci = quantile(boots.nputativeloci, 0.025)
-    row.maxconf_nloci = quantile(boots.nputativeloci, 0.975)
-    # Correlations
-    row.empirical_cor_fitness = cor(empirical_offset.offsets, minuslogfitness)
-    row.causal_cor_fitness = cor(causal_offset.offsets, minuslogfitness)
-    boot_cors = [cor(col, minuslogfitness) for col in eachcol(boots.offsets)]
-    row.minconf_cor_fitness = quantile(boot_cors, 0.025)
-    row.maxconf_cor_fitness = quantile(boot_cors, 0.975)
 end
 
 function fix_loci(loci, removed)
@@ -140,8 +17,38 @@ function fix_loci(loci, removed)
     new
 end
 
+function measure_time(func, args...)
+    result = func(args...)
+    elapsed_time = @elapsed func(args...)
+    return result, elapsed_time
+end
+
+function find_candidates(Y, X, significance)
+    mx = mean(X, dims=1)
+    sdx = std(X, dims=1)
+    X = (X .- mx) ./ sdx
+    Y = Y .- mean(Y, dims=1)
+    eigenvalues = eigvals(Y * Y' / (size(Y, 1) - 1))
+    _, pvalues = TracyWidom(eigenvalues)
+    K = max(findfirst(pvalues .> 0.01) - 1, 1)
+    model = RidgeLFMM(Y, X, K; center=false)
+    pvalues = LFMM_Ftest(model, Y, X; genomic_control=true, center=false)
+    qvalues = MultipleTesting.adjust(pvalues, BenjaminiHochberg())
+    findall(qvalues .< significance)
+end
+
+function empirical_offset(::Type{GO}, Y, X, Xpred, significance) where {GO<:GenomicOffsets.AbstractGO}
+    candidates = find_candidates(Y, X, significance)
+    genomic_offset(GenomicOffsets.fit(GO, Y[:, candidates], X), X, Xpred)
+end
+
+function empirical_offset(::Type{GeometricGO}, Y, X, Xpred, significance)
+    candidates = find_candidates(Y, X, significance)
+    genomic_offset(GenomicOffsets.fit(GeometricGO, Y, X), X, Xpred, candidates)
+end
+
 function handle_file(infile)
-    log("Processing $infile")
+    logging("Processing $infile")
     # Use R code from other scripts for simplicity
     R"""
     simulation <- read_rds($infile)
@@ -170,19 +77,113 @@ function handle_file(infile)
     zero_var = findall(sds .== 0)
     causal_loci = fix_loci(causal_loci, zero_var)
     Y = Y[:, vec(sds .> 0)]
-    log("Loading data with $(size(Y, 2)) loci")
-    df = grid_conditions(infile)
-    log("Testing $(size(df, 1)) conditions")
+    logging("Loading data with $(size(Y, 2)) loci")
 
-    for row in eachrow(df)
-        log("Processing condition $(row.FTest_FDR) $(row.TW_threshold) $(row.lambda)")
-        handle_row!(row, Y, X, Xstar, minuslogfitness, causal_loci)
+    df = DataFrame(
+        file=String[], method=String[], FDR=Float64[],
+        causal=Float64[], causalruntime=Float64[],
+        empirical=Float64[], empiricalruntime=Float64[],
+        minconf95=Float64[], maxconf95=Float64[], bootruntime=Float64[]
+    )
+    significances = [0.10, 0.05, 0.01]
+
+    # RONA
+    logging("Running RONA...")
+    causal, causalruntime = measure_time(() -> genomic_offset(GenomicOffsets.fit(RONA, Y[:, causal_loci], X), X, Xstar) |> vec)
+    for significance in significances
+        empiricaloffset, empiricalruntime = measure_time(() -> empirical_offset(RONA, Y, X, Xstar, significance) |> vec)
+        boots, bootruntime = measure_time(() -> bootstrap_with_candidates(
+            RONA, Xoshiro(1000), Y, X, Xstar, 100, candidates_threshold=significance)
+        )
+        bootscorrelation = [cor(boot, minuslogfitness) for boot in eachcol(boots)]
+        push!(df, Dict(
+            :file => infile, :method => "RONA", :FDR => significance,
+            :causal => cor(causal, minuslogfitness),
+            :causalruntime => causalruntime,
+            :empirical => cor(empiricaloffset, minuslogfitness),
+            :empiricalruntime => empiricalruntime,
+            :minconf95 => quantile(bootscorrelation, 0.025),
+            :maxconf95 => quantile(bootscorrelation, 0.975),
+            :bootruntime => bootruntime
+        )
+        )
+    end
+    # RDA
+    logging("Running RDA...")
+    causal, causalruntime = measure_time(() -> genomic_offset(GenomicOffsets.fit(RDAGO, Y[:, causal_loci], X), X, Xstar) |> vec)
+    for significance in significances
+        empiricaloffset, empiricalruntime = measure_time(() -> empirical_offset(RDAGO, Y, X, Xstar, significance) |> vec)
+        boots, bootruntime = measure_time(() -> bootstrap_with_candidates(
+            RDAGO, Xoshiro(1000), Y, X, Xstar, 100, candidates_threshold=significance)
+        )
+        bootscorrelation = [cor(boot, minuslogfitness) for boot in eachcol(boots)]
+        push!(df, Dict(
+            :file => infile, :method => "RDAGO", :FDR => significance,
+            :causal => cor(causal, minuslogfitness),
+            :causalruntime => causalruntime,
+            :empirical => cor(empiricaloffset, minuslogfitness),
+            :empiricalruntime => empiricalruntime,
+            :minconf95 => quantile(bootscorrelation, 0.025),
+            :maxconf95 => quantile(bootscorrelation, 0.975),
+            :bootruntime => bootruntime
+        )
+        )
+    end
+    # Geometric GO
+    logging("Running Geometric...")
+    causal, causalruntime = measure_time(() -> genomic_offset(GenomicOffsets.fit(GeometricGO, Y[:, causal_loci], X), X, Xstar) |> vec)
+    for significance in significances
+        empiricaloffset, empiricalruntime = measure_time(() -> empirical_offset(GeometricGO, Y, X, Xstar, significance) |> vec)
+        boots, bootruntime = measure_time(() -> bootstrap_with_candidates(
+            GeometricGO, Xoshiro(1000), Y, X, Xstar, 100, candidates_threshold=significance)
+        )
+        bootscorrelation = [cor(boot, minuslogfitness) for boot in eachcol(boots)]
+        push!(df, Dict(
+            :file => infile, :method => "GeometricGO", :FDR => significance,
+            :causal => cor(causal, minuslogfitness),
+            :causalruntime => causalruntime,
+            :empirical => cor(empiricaloffset, minuslogfitness),
+            :empiricalruntime => empiricalruntime,
+            :minconf95 => quantile(bootscorrelation, 0.025),
+            :maxconf95 => quantile(bootscorrelation, 0.975),
+            :bootruntime => bootruntime
+        )
+        )
+    end
+    # Gradient Forest GO
+    logging("Running GF...")
+    # We are going to discard extrapolated individuals
+    # Find individuals in Xstar that are not in range of X
+    out_of_range_individuals = [i for i in 1:size(Xstar, 1) if any(Xstar[i, :] .< vec(minimum(X, dims=1)) .|| Xstar[i, :] .> vec(maximum(X, dims=1)))]
+    inrange = setdiff(1:size(X, 1), out_of_range_individuals)
+    Y = Y[inrange, :]
+    X = X[inrange, :]
+    Xstar = Xstar[inrange, :]
+    minuslogfitness = minuslogfitness[inrange]
+    causal, causalruntime = measure_time(() -> genomic_offset(GenomicOffsets.fit(GradientForestGO, Y[:, causal_loci], X), X, Xstar) |> vec)
+    for significance in significances
+        empiricaloffset, empiricalruntime = measure_time(() -> empirical_offset(GradientForestGO, Y, X, Xstar, significance) |> vec)
+        boots, bootruntime = measure_time(() -> bootstrap_with_candidates(
+            GradientForestGO, Xoshiro(1000), Y, X, Xstar, 100, candidates_threshold=significance)
+        )
+        bootscorrelation = [cor(boot, minuslogfitness) for boot in eachcol(boots)]
+        push!(df, Dict(
+            :file => infile, :method => "GradientForestGO", :FDR => significance,
+            :causal => cor(causal, minuslogfitness),
+            :causalruntime => causalruntime,
+            :empirical => cor(empiricaloffset, minuslogfitness),
+            :empiricalruntime => empiricalruntime,
+            :minconf95 => quantile(bootscorrelation, 0.025),
+            :maxconf95 => quantile(bootscorrelation, 0.975),
+            :bootruntime => bootruntime
+        )
+        )
     end
     df
 end
 
 function run(infiles, outfile)
-    dfs = map(handle_file, infiles);
+    dfs = map(handle_file, infiles)
     df = vcat(dfs...)
     CSV.write(outfile, df; compress=true)
 end
--- workflow/rules/quality_control.smk
+++ workflow/rules/quality_control.smk
@@ -20,19 +20,20 @@ rule quality_control:
 rule quality_control_boots:
     input:
         files=expand(
-            "steps/slim/{model}_s{seed}_nQTL1s{n}_nQTL2s_{n}.Rds",
-            seed=range(100, 110),
-            n=[20, 50, 100],
-            model=["m3.1", "m3.2"],
+             "steps/slim/{model}_s{seed}_nQTL1s{n}_nQTL2s_{n}.Rds",
+             seed=range(100, 110),
+             n=[20, 100],
+             model=["m3.2"],
         ),
         script="scripts/02_local_adaptation_scenarios_bootstraps.jl",
     output:
         "results/local_adaptation_scenarios/quality_control_bootstrapping.csv.gz",
     log:
         "logs/local_adaptation_scenarios/quality_control_bootstrapping.log",
+    threads: 10
     resources:
         mem_mb=16000,
-        runtime=400,
+        runtime=350,
     shell:
         "julia -t {threads} --project=. {input.script} {input.files} {output} > {log} 2> {log}"
 
